4bde72b2d552846ed12554f9c9614d9fb05c2ec0
Roman Sergeev <sergeev.rs08@gmail.com>
2022-11-11 11:54:18 +0300

    initial commit

---
+++ src/main/java/com/romantest/autocredit/AutocreditApplication.java
@@ -0,0 +1,13 @@
+package com.romantest.autocredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romantest/autocredit/AutocreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantest.autocredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
