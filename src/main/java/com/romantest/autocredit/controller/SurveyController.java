d454f0c7e2a7470eefd1dc342b6d806aa483bf4a
Roman Sergeev <sergeev.rs08@gmail.com>
2022-11-13 13:34:10 +0300

    Saving survey form to the database

--- src/main/java/com/romantest/autocredit/controller/SurveyController.java
+++ src/main/java/com/romantest/autocredit/controller/SurveyController.java
@@ -1,10 +1,13 @@
 package com.romantest.autocredit.controller;
 
-import com.romantest.autocredit.entity.*;
+import com.romantest.autocredit.entity.Survey;
+import com.romantest.autocredit.service.SurveyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
@@ -13,21 +16,25 @@ import javax.validation.Valid;
 @SessionAttributes("survey")
 public class SurveyController {
 
+    @Autowired
+    SurveyService surveyService;
+
     @GetMapping
     public String surveyForm(){
         return "surveyForm";
     }
 
     @PostMapping
-    public String newSurvey(@Valid Survey survey,
-                            BindingResult bindingResult,
-                            Errors errors){
+    public String newSurvey(@Valid Survey survey, BindingResult bindingResult,
+                            Errors errors, SessionStatus sessionStatus){
+
         if(errors.hasErrors()){
-            System.out.println(errors.getAllErrors().toString());
             return "surveyForm";
         }
 
-        System.out.println(survey);
+        surveyService.save(survey);
+
+        sessionStatus.setComplete();
         return "redirect:/survey";
     }
 
