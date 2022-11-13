23466003b21aaeef01c26e52ee3d6e670ba56680
Roman Sergeev <sergeev.rs08@gmail.com>
2022-11-13 13:29:50 +0300

    entity values mapping

--- src/main/java/com/romantest/autocredit/entity/Organisation.java
+++ src/main/java/com/romantest/autocredit/entity/Organisation.java
@@ -3,10 +3,7 @@ package com.romantest.autocredit.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -17,7 +14,13 @@ import javax.validation.constraints.Size;
 public class Organisation {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    int id;
+
     @NotBlank(message = "Укажите название организации")
     @Size(min = 3, message = "Название организации должно состоять как минимум из 3 букв")
+    @Column(name = "name")
     String name;
+
 }
--- src/main/java/com/romantest/autocredit/entity/Position.java
+++ src/main/java/com/romantest/autocredit/entity/Position.java
@@ -1,6 +1,5 @@
 package com.romantest.autocredit.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,25 +12,20 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @Table(name = "position")
-@IdClass(PositionId.class)
 public class Position {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+
     @NotBlank(message = "Укажите наименование должности")
     @Size(min = 5, message = "Название должности должно состоять как минимум из пяти букв")
     String name;
 
-    @Id
-    @OneToOne
+
+    @OneToOne(targetEntity = Organisation.class)
     @Valid
     Organisation organisation;
 }
 
 
-@Getter
-@Setter
-@AllArgsConstructor
-class PositionId{
-    String name;
-    Organisation organisation;
-}
