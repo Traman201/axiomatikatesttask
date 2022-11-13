15d19fdae569c25d5c0b4fb843962508e6afe623
Roman Sergeev <sergeev.rs08@gmail.com>
2022-11-13 21:33:57 +0300

    marital status string format fix

--- src/main/java/com/romantest/autocredit/entity/MaritalStatus.java
+++ src/main/java/com/romantest/autocredit/entity/MaritalStatus.java
@@ -1,5 +1,7 @@
 package com.romantest.autocredit.entity;
 
+import com.romantest.autocredit.service.StringToMaritalStatusConverter;
+
 public enum MaritalStatus {
     SINGLE("1"),
     MARRIED("2"),
@@ -14,5 +16,9 @@ public enum MaritalStatus {
 
     public String getStatus(){
         return status;
+    }
+
+    public String convertStatus(){
+        return StringToMaritalStatusConverter.getMaritalStatus(this);
     }
 }
